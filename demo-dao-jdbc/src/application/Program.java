1ddbdeef7a2d13e97719414f44f7ba44231d8ff3
kaique oliveira santos <kaiquevicio@gmail.com>
2021-04-05 17:54:34 -0300

    Department entity class

---
+++ demo-dao-jdbc/src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+        Department obj = new Department(1, "Book");
+        System.out.println(obj);
+    }
+}
---
+++ demo-dao-jdbc/src/model/entities/Department.java
@@ -0,0 +1,56 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Department implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+
+    public Department(){}
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department that = (Department) o;
+        return getId().equals(that.getId()) && getName().equals(that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
