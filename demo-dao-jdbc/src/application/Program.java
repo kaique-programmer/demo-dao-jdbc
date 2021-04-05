6b2bfedc236449b92f40f6a95dc861010dadd09e
kaique oliveira santos <kaiquevicio@gmail.com>
2021-04-05 18:27:37 -0300

    SellerDaoJDBC and DaoFactory

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,7 @@ public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Book");
         Seller seller = new Seller(21, "kaique", "kaique@gmail.com", new Date(), 8000.0, department);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(department);
         System.out.println(seller);
     }
