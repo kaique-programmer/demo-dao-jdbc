c31d0394a2b4676a88e47f8cf04ae6b28b3d7674
kaique oliveira santos <kaiquevicio@gmail.com>
2021-04-05 18:02:28 -0300

    Seller entity class

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Book");
-        System.out.println(obj);
+        Department department = new Department(1, "Book");
+        Seller seller = new Seller(21, "kaique", "kaique@gmail.com", new Date(), 8000.0, department);
+        System.out.println(department);
+        System.out.println(seller);
     }
 }
